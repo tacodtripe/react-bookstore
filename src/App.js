6d2c016d2466d7746778f06c9de6a0b6b52ae8fc
Raul Figueroa <rfigueroa17@hotmail.com>
2021-12-16 11:51:58 -0700

    set up project configuration

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <h1>Hello</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
